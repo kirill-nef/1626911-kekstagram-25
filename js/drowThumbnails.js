0eae3ef5ce75ae7a882d74c62284a3967fcec196
Kirill <kirill-nef@mail.ru>
2022-04-11 11:13:26 +0500

    1
    1

--- js/drowThumbnails.js
+++ js/drowThumbnails.js
@@ -5,35 +5,37 @@ import { openBigPicture } from './openBigPicture.js';
 import { closePopup } from './formWork.js';
 import { setUserForSubmit } from './formWorkValidation.js';
 
+// Блок в который будем вставлять шаблоны template
+const listPictures = document.querySelector('.pictures');
+// Поиск template
+const templatePicture = document.querySelector('#picture').content;
+// Поиск внутри template блок picture
+const templatePictureItem = templatePicture.querySelector('.picture');
+// Сколько картинок создать
+
+// Функция отрисовки миниатюр на странице
+const drow = function (img, comments, likes, index) {
+  // Делаем полный клон блока
+  const taskPicture = templatePictureItem.cloneNode(true);
+  // Задаем src для картинки
+  const taskPictureImg = taskPicture.querySelector('img');
+  taskPictureImg.src = img;
+  // Задаем комментарий в picture__comments
+  const taskPictureComments = taskPicture.querySelector('.picture__comments');
+  taskPictureComments.textContent = comments;
+  // Задаем лайки в picture__likes
+  const taskPictureLikes = taskPicture.querySelector('.picture__likes');
+  taskPictureLikes.textContent = likes;
+  // Пушим на страницу
+  listPictures.appendChild(taskPicture);
+
+  taskPicture.dataset.index = index;
+};
+
 // Функция создания миниатюр вызывается из photoDataArray при успешной загрузки данных с сервера
 function drowThumbnails (photoDataArray) {
-  // Блок в который будем вставлять шаблоны template
-  const listPictures = document.querySelector('.pictures');
-  // Поиск template
-  const templatePicture = document.querySelector('#picture').content;
-  // Поиск внутри template блок picture
-  const templatePictureItem = templatePicture.querySelector('.picture');
-  // Сколько картинок создать
+
   const THUMBNAILS = photoDataArray.length;
-
-  // Функция создания миниатюр на странице
-  const drow = function (img, comments, likes, index) {
-    // Делаем полный клон блока
-    const taskPicture = templatePictureItem.cloneNode(true);
-    // Задаем src для картинки
-    const taskPictureImg = taskPicture.querySelector('img');
-    taskPictureImg.src = img;
-    // Задаем комментарий в picture__comments
-    const taskPictureComments = taskPicture.querySelector('.picture__comments');
-    taskPictureComments.textContent = comments;
-    // Задаем лайки в picture__likes
-    const taskPictureLikes = taskPicture.querySelector('.picture__likes');
-    taskPictureLikes.textContent = likes;
-    // Пушим на страницу
-    listPictures.appendChild(taskPicture);
-
-    taskPicture.dataset.index = index;
-  };
 
   // Цикл для парсинга массива photoDataArray
   for (let i = 0; i < THUMBNAILS; i++) {
